58d27016ac3d41274cf3c9d5bae09d0ed0f20d71
AvgustIS <abdunabiisaali2@gmail.com>
2026-04-08 09:04:04 +0500

    README 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,6 @@ public class Main {
     public static void main(String[] args) {
         BankSystem system = new BankSystem();
         system.start();
-        BankAccount[] accountsArray = new BankAccount[3];
 
-        accountsArray[0] = new BankAccount("BA1001", "Ali", 150000);
-        accountsArray[1] = new BankAccount("BA1002", "Sara", 220000);
-        accountsArray[2] = new BankAccount("BA1003", "Omar", 75000);
-
-        System.out.println("Accounts List:");
-        for (int i = 0; i < accountsArray.length; i++) {
-            System.out.println((i + 1) + ". " + accountsArray[i].getUsername() +
-                    " – Balance: " + accountsArray[i].getBalance());
-        }
     }
 }
